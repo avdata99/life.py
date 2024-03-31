5327a7796ff5f02879eea9ae14374c1ee2f8d753
avdata99 <andres@data99.com.ar>
2024-03-31 11:21:24 -0300

    Notes

--- src/life/files/__init__.py
+++ src/life/files/__init__.py
@@ -1,5 +1,5 @@
 from life.files.bing import BinGenFile
 from life.files.fasta import FastaFile
 
-# TODO implement a plugin system
+# TODO implement a plugin system with pkg_resources.iter_entry_points
 ALL_FILE_CLASSES = [BinGenFile, FastaFile]
