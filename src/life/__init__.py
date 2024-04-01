e243aac0555f0239c3bfe2007a574f1d892d31e0
avdata99 <andres@data99.com.ar>
2024-04-01 20:35:10 -0300

    Preparing to build

--- src/life/__init__.py
+++ src/life/__init__.py
@@ -2,3 +2,6 @@ from life.nucleotide import Nucleotide  # noqa F401
 from life.gene import Gene  # noqa F401
 from life.codon import Codon  # noqa F401
 from life.aminoacid import Aminoacid  # noqa F401
+
+
+__version__ = '0.1.0'
